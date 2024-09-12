8028324b3c940f70a713b36959af73e95fdbcb5b
Parsue Choi <parsue@gmail.com>
2024-09-12 09:08:22 +0800

    Create UIButtonType.cs

---
+++ Runtime/State/UIButtonType.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AceLand.Input.Mono
+{
+    [Serializable]
+    public enum UIButtonType
+    {
+        None = 0,
+        UIAction = 10,
+        PhysicsButton = 20,
+    }
+}
