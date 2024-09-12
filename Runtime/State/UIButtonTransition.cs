3693b3f4bd70ebaaeef04114c86d65c1fb81c883
Parsue Choi <parsue@gmail.com>
2024-09-12 09:08:37 +0800

    Create UIButtonTransition.cs

---
+++ Runtime/State/UIButtonTransition.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace AceLand.Input.Mono
+{
+    [Serializable]
+    public enum UIButtonTransition
+    {
+        None = 0,
+        Color = 10,
+        Sprite = 20,
+        SpriteBundle = 30,
+    }
+}
